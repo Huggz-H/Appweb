a37cc5e1b51a568bb17419d98304e75ade4daaa6
Huggz-H <hugo.dasilva@eemi.com>
2024-09-26 14:14:05 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,2 +1,10 @@
 import streamlit as st 
+
+# Titre 
 st.title("Mon formulaire")
+
+# Texte 
+st.write("ceci est un formulaire de contact")
+
+#champ de saisi 
+st.text_input("Tapez votre texte : ")
