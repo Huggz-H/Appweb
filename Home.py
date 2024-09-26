89040df83b8a3021ecaa0e229131b4f713fae58f
Huggz-H <hugo.dasilva@eemi.com>
2024-09-26 15:29:26 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -27,7 +27,6 @@ st.selectbox ("Selectionnez votre niveau d'étude", ["Bac", "Bac+2","Bac+5"])
 age = st.select_slider("Quel est votre âge ?", range(0, 99))
 
 #Conditions en python 
-
 age > 18: 
   st.write ("Vous êtes majeur")
 else:
