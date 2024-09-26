4e1a7cc626ae48969859ceafc986eca6b08d474d
Huggz-H <hugo.dasilva@eemi.com>
2024-09-26 14:38:17 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,3 +16,12 @@ st.image ("https://cdn-europe1.lanmedia.fr/var/europe1/storage/images/europe1/cu
 
 #sidebar
 st.sidebar.title ("Hugo Da Silva")
+ 
+# Vidéo dans la sidebar
+st.sidebar.video("https://www.youtube.com/watch?v=mu5SSkn23lY")
+
+#Select bar 
+st.selectbox(Selectionnez votre niveau d'étude", ["Bar", "Bar+2", Bac+5"]
+
+#Selexr slider
+age = st.select_slider("Quel est votre âge ?", range(0, 99)
