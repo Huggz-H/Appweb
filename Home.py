9dccea9abe95c98278e1465e608db894651f2c90
Huggz-H <hugo.dasilva@eemi.com>
2024-09-26 14:39:04 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ st.sidebar.title ("Hugo Da Silva")
 st.sidebar.video("https://www.youtube.com/watch?v=mu5SSkn23lY")
 
 #Select bar 
-st.selectbox(Selectionnez votre niveau d'étude", ["Bar", "Bar+2", Bac+5"]
+st.selectbox(Selectionnez votre niveau d'étude", ["Bac", "Bac+2", Bac+5"])
 
 #Selexr slider
 age = st.select_slider("Quel est votre âge ?", range(0, 99)
